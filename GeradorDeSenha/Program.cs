787d5f0c74dd76cd0374e037d466dbce00182a0f
Gabriel David <devgabrieldavid@gmail.com>
2023-12-19 19:46:34 -0300

    Feito a base da senha com os elementos fundamentais

--- GeradorDeSenha/Program.cs
+++ GeradorDeSenha/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace GeradorDeSenha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            // Gera um número aleatório de 0 a 9
+            int numeroAleatorio = random.Next(0, 10);
+
+            // Gera uma letra aleatória de 'A' até 'Z' maiuscula e depois minuscula
+            char letraAleatoria = (char)('A' + random.Next(26));
+            string maiuscula = letraAleatoria.ToString().ToUpper();
+
+            letraAleatoria = (char)('A' + random.Next(26));
+            string minuscula = letraAleatoria.ToString().ToLower();
+
+            // Gera um caractere especial aleatório: ! @ # $ % ^ & * ( ) _ + - = { } [ ] | \ : ; " ' < > , . ? /
+            string[] caracteresEspeciais = new string[30];
+            caracteresEspeciais = "! @ # $ % ^ & * ( ) _ + - = { } [ ] | \\ : ; \" ' < > , . ? /".Split(' ');
+            int caractereEspecialAleatorio = random.Next(0, 30);
+            string caractereEscolhido = caracteresEspeciais[caractereEspecialAleatorio];
+
+            // Cria a base da senha com os elementos fundamentais
+            string senha = maiuscula + minuscula + caractereEscolhido + numeroAleatorio;
+
+            // Exibe a senha
+            Console.WriteLine($"Senha gerada: {senha}");
+
+        }
+    }
+}
